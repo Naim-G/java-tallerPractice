7033b118e9cc90fc5b2631d5d137528efd174c84
java-tallerPractice <naim.godoy10@gmail.com>
2023-04-08 14:39:47 -0400

    Taller practica terminado

---
+++ empty/src/cl/ucn/disc/pa/empty/Main.java
@@ -0,0 +1,23 @@
+package cl.ucn.disc.pa.empty;
+
+import edu.princeton.cs.stdlib.StdOut;
+
+/**
+ * The Main class.
+ *
+ * @author Naim - Godoy Valdivia.
+ */
+public class Main {
+
+    /**
+     * The Main.
+     *
+     * @param args to use.
+     */
+    public static void main(String[] args) {
+
+        StdOut.println("Hello World");
+
+    }
+
+}
